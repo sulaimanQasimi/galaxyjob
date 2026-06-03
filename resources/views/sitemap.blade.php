a010f41b6647b52ac596cfe1c114da7da955511b
Sulaiman Qasimi <farzadqasimy@gmail.com>
2026-06-03 22:54:47 +0430

    Escape sitemap URLs as XML

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach ($urls as $url)
     <url>
-        <loc>{{ $url['loc'] }}</loc>
+        <loc>{!! htmlspecialchars($url['loc'], ENT_XML1, 'UTF-8') !!}</loc>
         @isset($url['lastmod'])
             <lastmod>{{ $url['lastmod'] }}</lastmod>
         @endisset
